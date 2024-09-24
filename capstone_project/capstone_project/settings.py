5f586b302bee6f8a8a8c39a73688a27acde3803b
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-09-24 02:20:07 -0300

    localhost_bdrender

--- capstone_project/capstone_project/settings.py
+++ capstone_project/capstone_project/settings.py
@@ -10,13 +10,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY =("u9p7wdUafTzzCnE9kQGCxvPBk7V1sYNo")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower == "true"
+#DEBUG = os.environ.get("DEBUG", "False").lower == "true"
+
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(" ")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1" ]
 
 
 
