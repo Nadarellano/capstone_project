393e4a3a1fb9e100f244a4278b416e607f98d357
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-09-28 16:52:29 -0300

    Upa

--- capstone_project/negocio/apps.py
+++ capstone_project/negocio/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NegocioConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "capstone_project.negocio"
+    name = "negocio"
