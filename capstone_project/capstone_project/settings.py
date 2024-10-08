c6f11f91862a2c59d15910380109feeec8e1c727
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-10-08 20:49:44 -0300

    settingsstorageolde

--- capstone_project/capstone_project/settings.py
+++ capstone_project/capstone_project/settings.py
@@ -144,12 +144,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    }
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Primary key field type
