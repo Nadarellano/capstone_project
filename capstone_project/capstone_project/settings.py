72a60d859b9213071a3c4a6073c6ab2526e6acd0
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-10-08 21:53:12 -0300

    settingddd

--- capstone_project/capstone_project/settings.py
+++ capstone_project/capstone_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-devel
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "True").lower() == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
